13ed874a1201042c5fd06485bf4621fadecc40d8
Jérôme Desboeufs <jerome@livingdata.co>
2023-06-21 14:23:45 +0200

    First addok-importer integration with poi

--- indexes/poi/scripts/build-index/index.js
+++ indexes/poi/scripts/build-index/index.js
@@ -3,8 +3,15 @@
 import 'dotenv/config.js'
 
 import path from 'node:path'
+import {createReadStream} from 'node:fs'
+import {Transform} from 'node:stream'
+
+import {parse, stringify} from 'ndjson'
+import {omit} from 'lodash-es'
+
+import {createImporter} from '../../../../lib/addok-importer.js'
 import {createIndexer} from '../../../../lib/spatial-index/indexer.js'
-import {POI_INDEX_MDB_BASE_PATH, POI_DATA_PATH} from '../../util/paths.js'
+import {POI_INDEX_MDB_BASE_PATH, POI_INDEX_PATH, POI_DATA_PATH} from '../../util/paths.js'
 import {extractFeatures} from './extract.js'
 
 const INPUT_FILE = path.join(POI_DATA_PATH, 'poi.ndjson')
@@ -28,4 +35,20 @@ await indexer.writeFeatures(extractFeatures(INPUT_FILE))
 
 clearInterval(writeFeaturesLoop)
 
+const addokImporter = await createImporter(POI_INDEX_PATH, './indexes/poi/config/addok.conf')
+
+// Importing into addok
+await addokImporter.batchImport(
+  createReadStream(INPUT_FILE)
+    .pipe(parse())
+    .pipe(new Transform({
+      transform(row, enc, cb) {
+        cb(null, omit(row, ['truegeometry', 'toponym']))
+      },
+      objectMode: true
+    }))
+    .pipe(stringify())
+)
+
 await indexer.finish()
+await addokImporter.finish()
