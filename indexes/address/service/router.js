3d6a5547fd8094680bba2692103795a80a8ce013
Jérôme Desboeufs <jerome@livingdata.co>
2023-06-21 16:47:36 +0200

    Add search in address/service using addok-cluster

--- indexes/address/service/router.js
+++ indexes/address/service/router.js
@@ -1,20 +1,33 @@
+import path from 'node:path'
 import {Router, json} from 'express'
+import {createCluster} from 'addok-cluster'
 
 import w from '../../../lib/w.js'
 import {createRtree} from '../../../lib/spatial-index/rtree.js'
+import {createInstance as createRedisServer} from '../../../lib/addok/redis.js'
 
-import {ADDRESS_INDEX_RTREE_PATH} from '../util/paths.js'
+import {ADDRESS_INDEX_RTREE_PATH, ADDRESS_INDEX_PATH} from '../util/paths.js'
 
 import {createDatabase} from './db.js'
-import {reverse} from './search.js'
+import {reverse} from './reverse.js'
 
 export async function createRouter() {
   const db = await createDatabase()
   const rtreeIndex = await createRtree(ADDRESS_INDEX_RTREE_PATH)
+  const redisServer = await createRedisServer(ADDRESS_INDEX_PATH)
+  const addokCluster = await createCluster({
+    addokRedisUrl: ['unix:' + redisServer.socketPath],
+    addokConfigModule: path.resolve('./indexes/address/config/addok.conf')
+  })
 
   const router = new Router()
 
   router.use(json())
+
+  router.post('/search', w(async (req, res) => {
+    const results = await addokCluster.geocode(req.body)
+    res.send(results)
+  }))
 
   router.post('/reverse', w((req, res) => {
     res.send(reverse({...req.body, db, rtreeIndex}))
