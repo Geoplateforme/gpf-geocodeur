6711ae8675ccf551790a51b30b09a8f5f18110dc
Cédric Pronzola <cedric@livingdata.co>
2023-06-23 15:40:55 +0400

    Create route /completion

--- api/router.js
+++ api/router.js
@@ -7,7 +7,9 @@ import errorHandler from '../lib/error-handler.js'
 import {createIndexes} from './indexes/index.js'
 import search from './operations/search.js'
 import reverse from './operations/reverse.js'
+import autocomplete from './operations/autocomplete.js'
 import {PARAMS, extractParams} from './params/base.js'
+import {extractParams as extractAutocompleteParams} from './params/autocomplete.js'
 
 const GEOCODE_INDEXES = process.env.GEOCODE_INDEXES
   ? process.env.GEOCODE_INDEXES.split(',')
@@ -33,6 +35,15 @@ export default function createRouter(options = {}) {
     res.send({
       type: 'FeatureCollection',
       features: results
+    })
+  }))
+
+  router.get('/completion', w(async (req, res) => {
+    const params = extractAutocompleteParams(req.query)
+    const results = await autocomplete(params, {indexes})
+    res.send({
+      status: res.statusCode === 200 ? 'OK' : res.statusCode,
+      results
     })
   }))
 
