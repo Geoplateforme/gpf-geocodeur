3c94dc064c75807dbeff97987b16b689a785574f
Guillaume <guillaume@livingdata.co>
2023-06-27 16:48:26 +0200

    Add test

---
+++ api/config/__tests__/compute-geocodage-capabilities.js
@@ -0,0 +1,16 @@
+import test from 'ava'
+import computeGeocodageCapabilities from '../compute-geocodage-capabilities.js'
+
+test('computeGeocodageCapabilities / Fields', t => {
+  const computedCapabilities = computeGeocodageCapabilities()
+  const searchOperations = computedCapabilities.operations[0]
+  const reverseOperations = computedCapabilities.operations[1]
+
+  t.truthy(searchOperations.parameters.find(k => k.name === 'sheet'))
+  t.truthy(reverseOperations.parameters.find(k => k.name === 'searchgeom'))
+  t.truthy(reverseOperations.parameters.find(k => k.name === 'sheet'))
+  t.is(searchOperations.parameters.find(k => k.name === 'searchgeom'), undefined)
+  t.is(searchOperations.id, 'search')
+  t.is(reverseOperations.id, 'reverse')
+})
+
