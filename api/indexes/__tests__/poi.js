c110784c36d4c4f31d25f6bebd6f967738569be6
Guillaume <guillaume@livingdata.co>
2023-07-03 10:15:04 +0200

    Test prepareRequest parcel

---
+++ api/indexes/__tests__/poi.js
@@ -0,0 +1,23 @@
+import test from 'ava'
+import {prepareRequest} from '../poi.js'
+
+const params = {
+  q: 'metz',
+  limit: 5,
+  autocomplete: true,
+  geometry: {
+    type: 'Point',
+    coordinates: [0, 0]
+  }
+}
+
+test('prepareRequest', t => {
+  const preparedRequest = prepareRequest(params)
+
+  console.log(preparedRequest)
+
+  t.is(preparedRequest.q, 'metz')
+  t.is(preparedRequest.center, undefined)
+  t.is(preparedRequest.limit, 5)
+  t.true(preparedRequest.autocomplete)
+})
