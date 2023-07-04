c4abf91381cf67021881bd5a870b0f178db27580
Jérôme Desboeufs <jerome@livingdata.co>
2023-07-04 18:11:18 +0200

    Add test to extractFeatures(poi)

--- indexes/poi/scripts/build-from-bdtopo/index.js
+++ indexes/poi/scripts/build-from-bdtopo/index.js
@@ -8,19 +8,14 @@ import {Readable} from 'node:stream'
 import {createWriteStream} from 'node:fs'
 import {rm, mkdir, writeFile} from 'node:fs/promises'
 
-import gdal from 'gdal-async'
-import {mapValues, isFunction, uniq, compact, chain} from 'lodash-es'
-import truncate from '@turf/truncate'
-import centroid from '@turf/centroid'
-
 import {downloadAndExtractToTmp, getArchiveURL, getPath} from '../../../../lib/geoservices.js'
-import {getCommune} from '../../../../lib/cog.js'
 
 import {POI_DATA_PATH, POI_DATA_CATEGORIES_PATH} from '../../util/paths.js'
 
 import {LAYERS, MAIN_CATEGORIES} from './mapping.js'
 import {createCommunesIndex} from './communes.js'
 import {createAccumulator} from './categories.js'
+import {extractFeatures} from './extract.js'
 
 const ALL_DEPARTEMENTS = [
   '01', '02', '03', '04', '05', '06', '07', '08', '09',
@@ -56,105 +51,6 @@ const COMPUTED_FIELDS_SCHEMA = {
   city: Array
 }
 
-function computeFields(originalProperties, fieldsDefinition) {
-  return mapValues(fieldsDefinition, (resolver, fieldName) => {
-    let resolvedValue = isFunction(resolver)
-      ? resolver(originalProperties)
-      : resolver
-
-    if (resolvedValue === undefined) {
-      return
-    }
-
-    if (COMPUTED_FIELDS_SCHEMA[fieldName] === Array) {
-      if (!Array.isArray(resolvedValue)) {
-        resolvedValue = [resolvedValue]
-      }
-
-      return uniq(compact(resolvedValue))
-    }
-
-    return resolvedValue
-  })
-}
-
-function * readFeatures(datasetPath, layersDefinitions, acc) {
-  const ds = gdal.open(datasetPath)
-  const wgs84 = gdal.SpatialReference.fromProj4('+init=epsg:4326')
-
-  for (const [layerName, config] of Object.entries(layersDefinitions)) {
-    let layer
-
-    try {
-      layer = ds.layers.get(layerName)
-    } catch {
-      continue
-    }
-
-    console.log(` * ${layerName}`)
-
-    const transformation = new gdal.CoordinateTransformation(layer.srs, wgs84)
-
-    for (const feature of layer.features) {
-      const properties = feature.fields.toObject()
-      const {cleabs} = properties
-
-      if (cleabsUniqIndex.has(cleabs)) {
-        continue
-      }
-
-      // eslint-disable-next-line unicorn/no-array-callback-reference
-      if (config.filter && !config.filter(properties)) {
-        continue
-      }
-
-      const fields = computeFields(properties, config.fields)
-
-      fields.importance = fields.classification
-        ? Math.round((1 - ((fields.classification - 1) * 0.1)) * 100) / 100
-        : 0.4
-
-      fields.extrafields = {
-        cleabs
-      }
-
-      let geometry = feature.getGeometry()
-      geometry.transform(transformation)
-
-      if (config.simplification) {
-        geometry = geometry.simplifyPreserveTopology(config.simplification)
-      }
-
-      if (config.computeCommunes) {
-        const citycode = communesIndex.getIntersectingCommunes(geometry)
-        const communes = citycode.map(c => getCommune(c)).filter(Boolean)
-
-        const depcode = chain(communes).map('departement').compact().uniq().value()
-        const postcode = chain(communes).map('codesPostaux').compact().flatten().uniq().value()
-        const city = chain(communes).map('nom').compact().uniq().value()
-
-        fields.citycode = [...citycode, ...depcode]
-        fields.city = city
-        fields.postcode = postcode
-      }
-
-      const truncatedGeometry = truncate(geometry.toObject(), {mutate: true, coordinates: 2})
-      const truncatedCentroid = truncate(centroid(truncatedGeometry), {mutate: true})
-      const {geometry: {coordinates: [lon, lat]}} = truncatedCentroid
-
-      fields.truegeometry = JSON.stringify(truncatedGeometry)
-      fields.lon = lon
-      fields.lat = lat
-
-      acc.addCategories(fields.category)
-
-      yield JSON.stringify(fields) + '\n'
-    }
-  }
-
-  ds.close()
-}
-
 await mkdir(POI_DATA_PATH, {recursive: true})
 const outputFile = createWriteStream(path.join(POI_DATA_PATH, 'poi.ndjson'), {encoding: 'utf8'})
 
@@ -165,7 +61,15 @@ for (const codeDepartement of DEPARTEMENTS) {
   const archiveDirPath = await downloadAndExtractToTmp(archiveUrl)
   const datasetPath = await getPath(archiveDirPath, 'BDT_3-3_GPKG_*.gpkg')
 
-  const featureStream = Readable.from(readFeatures(datasetPath, LAYERS, categoriesAccumulator))
+  const featureStream = Readable.from(extractFeatures({
+    datasetPath,
+    computedFieldsSchema: COMPUTED_FIELDS_SCHEMA,
+    layersDefinitions: LAYERS,
+    cleabsUniqIndex,
+    communesIndex,
+    categoriesAccumulator
+  }))
+
   featureStream.pipe(outputFile, {end: false})
   await finished(featureStream)
 
