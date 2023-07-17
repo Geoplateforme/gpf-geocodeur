e17e6fa0506c8302402c57cb183c3cb66791688b
Jérôme Desboeufs <jerome@livingdata.co>
2023-07-17 12:28:39 +0200

    Fix parcel/transformParcel

--- indexes/parcel/scripts/build-index/__tests__/transform.js
+++ indexes/parcel/scripts/build-index/__tests__/transform.js
@@ -2,36 +2,18 @@
 import test from 'ava'
 import {transformParcel} from '../transform.js'
 
-const aFeature = {
-  properties: {
-    IDU: 'featureId',
-    CODE_DEP: 55,
-    CODE_COM: 55200,
-    COM_ABS: 55500,
-    CODE_ARR: 55120,
-    SECTION: 123,
-    FEUILLE: 4,
-    NUMERO: 1
-  },
-  geometry: {
-    type: 'Polygon',
-    coordinates: [
-      [
-        [-73.9876, 40.7661],
-        [-73.9877, 40.7661],
-        [-73.9877, 40.7662],
-        [-73.9876, 40.7662],
-        [-73.9876, 40.7661]
-      ]
-    ]
-  }
-}
-
 test('transformParcel', t => {
-  const transformedFeature = transformParcel(aFeature)
-
-  t.deepEqual(transformedFeature, {
-    type: 'Feature',
+  const originalFeature = {
+    properties: {
+      IDU: 'featureId',
+      CODE_DEP: '55',
+      CODE_COM: '501',
+      COM_ABS: '511',
+      CODE_ARR: '101',
+      SECTION: '0A',
+      FEUILLE: 4,
+      NUMERO: '0001'
+    },
     geometry: {
       type: 'Polygon',
       coordinates: [
@@ -43,18 +25,34 @@ test('transformParcel', t => {
           [-73.9876, 40.7661]
         ]
       ]
-    },
+    }
+  }
+
+  t.deepEqual(transformParcel(originalFeature), {
+    type: 'Feature',
     properties: {
       id: 'featureId',
-      departmentcode: 55,
-      municipalitycode: 55200,
-      oldmunicipalitycode: 55500,
-      districtcode: 55120,
-      section: 123,
+      departmentcode: '55',
+      municipalitycode: '501',
+      oldmunicipalitycode: '511',
+      districtcode: '101',
+      section: '0A',
       sheet: '04',
-      number: 1,
+      number: '0001',
       lon: -73.98765,
       lat: 40.766149999999996
+    },
+    geometry: {
+      type: 'Polygon',
+      coordinates: [
+        [
+          [-73.9876, 40.7661],
+          [-73.9877, 40.7661],
+          [-73.9877, 40.7662],
+          [-73.9876, 40.7662],
+          [-73.9876, 40.7661]
+        ]
+      ]
     }
   })
 })
