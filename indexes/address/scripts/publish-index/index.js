0429bc10faf6362b7d8c74d0db365eb5ac46383b
Jérôme Desboeufs <jerome@livingdata.co>
2023-07-25 23:50:45 +0200

    Add publish-index scripts

---
+++ indexes/address/scripts/publish-index/index.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'dotenv/config.js'
+import path from 'node:path'
+
+import {packAndUpload} from '../../../../lib/scripts/publish-index/index.js'
+
+await packAndUpload('address', path.resolve('./data/address/index'))
