617d1472532cf69fc6f0206de38390863066000d
Cédric Pronzola <cedric@livingdata.co>
2023-08-17 17:37:21 +0400

    Add cache to Open API editConfig

--- api/open-api/edit-config.js
+++ api/open-api/edit-config.js
@@ -1,6 +1,19 @@
 import {readFile} from 'node:fs/promises'
 
+let _openApiConfig = null
+let _openApiConfigDate = null
+
+const FIVE_MINUTES = 5 * 60 * 1000
+
 export async function editConfig(yamlPath, apiUrl) {
-  const yamlContent = await readFile(yamlPath, {encoding: 'utf8'})
-  return yamlContent.replace('$API_URL', apiUrl)
+  if (_openApiConfig && (Date.now() - _openApiConfigDate < FIVE_MINUTES)) {
+    return _openApiConfig.replace('$API_URL', apiUrl)
+  }
+
+  const openApiConfig = await readFile(yamlPath, {encoding: 'utf8'})
+
+  _openApiConfig = openApiConfig
+  _openApiConfigDate = Date.now()
+
+  return openApiConfig.replace('$API_URL', apiUrl)
 }
